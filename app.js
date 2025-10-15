2748f84f6290954d3154bbb7f46bb6ac357287fd
Dave-Rushabh <rushabh.dave.dev@gmail.com>
2025-10-15 13:30:18 +0530

    Init APIs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const connectToDatabase = require("./src/config/database");
 const app = express();
 require("dotenv").config();
 const userModel = require("./src/models/user");
+app.use(express.json());
 
 connectToDatabase()
   .then(() => {
@@ -17,21 +18,81 @@ connectToDatabase()
   });
 
 app.post("/sign-up", async (req, res) => {
-  const dummyUser = {
-    firstName: "virat",
-    lastName: "Doe",
-    emailId: "john.do22e@example2.com",
-    password: "password123",
-    age: 30,
-    gender: "Male",
-  };
-
+  const dummyUser = req.body;
   const newUser = new userModel(dummyUser);
 
   try {
     await newUser.save();
     res.status(201).send("User created successfully");
   } catch (error) {
+    console.log(error, "error");
     res.status(500).send("Error creating user");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const { emailId } = req.body;
+    const user = await userModel.findOne({ emailId });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    console.error(error, "error");
+    res.status(500).send("Error fetching users");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).send("Error fetching feed");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const { userId } = req.body;
+
+    const deletedUser = await userModel.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).send("User not found");
+    }
+
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).send("Error deleting user");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const { userId, updatedData } = req.body;
+
+    console.log(userId, updatedData, "userId, updatedData");
+    const updatedUser = await userModel.findByIdAndUpdate(
+      { _id: userId },
+      updatedData,
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).send("User not found");
+    } else {
+      return res.status(200).json(updatedUser);
+    }
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).send("Error updating user");
   }
 });
